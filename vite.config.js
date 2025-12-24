a7ac569dc5edcfb1053e6062003529e8058bdc76
Soyel Rana <soyelrana224@gmail.com>
2025-12-24 16:01:50 +0530

    added api proxy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // optional
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://vision-backend-328443733915.asia-south2.run.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
 });
